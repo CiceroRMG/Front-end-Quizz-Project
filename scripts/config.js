2baababa610944ecd9a18192d6ad0bb0519ecc2e
Cicero Gomes <cicero.ufsc@gmail.com>
2024-09-23 11:58:11 -0300

    altera basedUrl front

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,2 @@
-export const based_url = `https://transcendent-snickerdoodle-d6507f.netlify.app`
+export const based_url = `https://polvoquiz.netlify.app`
 export const baseUrl = `https://back-end-quizz-project.onrender.com`
